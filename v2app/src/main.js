6148e9b911b9cd6f844206dc8bd77b66cff52a6a
yanzhi <tyktianxia@gmail.com>
2022-04-13 23:59:41 +0800

    添加vuex

--- v2app/src/main.js
+++ v2app/src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./router";
+import store from './store'
 
 import './util/vuels.js'
 
-import router from "./router";
-
 Vue.config.productionTip = false
-
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
