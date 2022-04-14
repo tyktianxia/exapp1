ff44ab562c2a7bafd039aa92fd21d0474a4637ec
tanyukang <tanyukang888@163.com>
2022-04-14 10:17:27 +0800

    1

--- v2app/src/App.vue
+++ v2app/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <p v-for="(item, index) in arrList" :key="index">{{ item }}</p>
+    <button @click="fun1">click</button>
   </div>
 </template>
 
@@ -8,10 +10,53 @@
 export default {
   name: "App",
   data() {
-    return {};
+    return {
+      arrList: ["A", "B", "C", "D"],
+      age: 12,
+      a: 0,
+      b: 2,
+      arr: [],
+    };
+  },
+  watch: {
+    age(nval, oval) {
+      console.log("watch age", nval, oval);
+      this.age++;
+    },
+    async a() {
+      this.arr.push(this.c);
+      console.log("a");
+      await this.$nextTick();
+      if (this.arr.length > 0) {
+        this.arr[0]();
+        this.arr = [];
+      }
+    },
+    async b() {
+      this.arr.push(this.c);
+      console.log("b");
+      await this.$nextTick();
+      if (this.arr.length > 0) {
+        this.arr[0]();
+        this.arr = [];
+      }
+    },
   },
   created() {},
-  methods: {},
+  methods: {
+    // d() {
+    //   this.c();
+    // },
+    async c() {
+      console.log("c");
+    },
+    async fun1() {
+      this.age++;
+      // this.age++;
+      // this.a++;
+      // this.b++;
+    },
+  },
 };
 </script>
 
